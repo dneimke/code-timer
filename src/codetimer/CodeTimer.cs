a3038aff956fe2c2d6919540cf9938b2c7de4f65
Darren Neimke <darren.neimke@live.com>
2017-10-21 08:14:04 +1030

    removes tests, adds nuspec

--- src/codetimer/CodeTimer.cs
+++ src/codetimer/CodeTimer.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 using Microsoft.Extensions.Logging;
 
 namespace CodeTimer {
@@ -7,53 +9,92 @@ namespace CodeTimer {
 
         private readonly string name;
         private readonly ILogger logger;
+        private readonly Stopwatch timer;
+        private List<Segment> segments;
 
         public CodeTimer(string name)
         {
             this.name = name;
+            this.segments = new List<Segment>();
+            this.timer = new Stopwatch();
+            timer.Start();
         }
 
         public CodeTimer(string name, ILogger logger)
         {
             this.name = name;
             this.logger = logger;
+            this.segments = new List<Segment>();
+            this.timer = new Stopwatch();
+            timer.Start();
         }
 
         public long ExpectedMilliseconds { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
         public void Complete()
         {
-            throw new System.NotImplementedException();
+            this.timer.Stop();
+
+            if(this.logger != null) {
+
+                var logMessage = this.GetFormattedResult();
+
+                if(this.Success()) {
+                    logger.LogInformation(logMessage);
+                }else{
+                    logger.LogError(logMessage);
+                }
+            }
         }
 
         public long GetElapsedMilliseconds()
         {
-            throw new System.NotImplementedException();
+            return this.timer.ElapsedMilliseconds;
         }
 
         public string GetFormattedResult()
         {
-            throw new System.NotImplementedException();
+            var jobName = string.IsNullOrEmpty(this.name) ? "CodeTimer Job" : this.name;
+            var timeTaken = this.GetElapsedMilliseconds();
+            var result = this.Success() ? "succeeded" : "failed";
+
+            var formattedHeader = $"{jobName} ran for {timeTaken}ms and {result}";
+
+            var sb = new StringBuilder() ;
+
+            sb.AppendLine(formattedHeader);
+
+            foreach(var segment in this.segments) {
+                sb.AppendLine($"{segment.Name} timings - {segment.Ticks}");
+            }
+            var logMessage = sb.ToString();
+
+            return logMessage;
         }
 
         public IEnumerable<Segment> GetSegments()
         {
-            throw new System.NotImplementedException();
+            return this.segments;
         }
 
         public void Mark()
         {
-            throw new System.NotImplementedException();
+            this.segments.Add(new Segment(this.timer.ElapsedMilliseconds));
         }
 
         public void Mark(string segmentName)
         {
-            throw new System.NotImplementedException();
+            this.segments.Add(new Segment(this.timer.ElapsedMilliseconds, segmentName));
         }
 
         public bool Success()
         {
-            throw new System.NotImplementedException();
+            if(this.ExpectedMilliseconds > 0)
+            {
+                return this.GetElapsedMilliseconds() <= this.ExpectedMilliseconds;
+            }
+
+            return true;
         }
     }
 }
